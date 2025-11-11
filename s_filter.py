d09e8cbf9e9f0cddc6661756efca2675aa78847a
Iswb04 <isabellaramos654@gmail.com>
2025-11-11 14:38:30 -0300

    111125

---
+++ s_filter.py
@@ -0,0 +1,21 @@
+import sqlite3
+from s_main import DB_FILE, TABLE_NAME
+
+connection = sqlite3.connect(DB_FILE)
+cursor = connection.cursor()
+
+cursor.execute(
+    f'''SELECT NAME,AGE FROM {TABLE_NAME} 
+    WHERE AGE >= 18 AND AGE <= 50'''
+) 
+
+
+rows = cursor.fetchall() 
+if not rows:
+    print('Nenhum registro encontrado!')
+else:
+    for row in rows: # row = linha
+        print(row)
+
+cursor.close()
+connection.close()
